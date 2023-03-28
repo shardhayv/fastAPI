d3ef00e2f42d5bd0591c6c433eb1af470765acfc
shardhayv <shardhayvatshyayan7@gmail.com>
2023-03-28 08:05:49 +0545

    HTTPException for status_code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randrange
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -33,7 +33,7 @@ async def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 100000000)
@@ -48,8 +48,12 @@ def latest_post():
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Post with the id: {id} does not exist')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"data": f'Post with the id: {id} does not exist'}
     print(post)
-
     return {"post_detail": post}
