96f9f37608c2ab379eecfe8012d6d99632b45c12
shardhayv <shardhayvatshyayan7@gmail.com>
2023-03-28 08:27:40 +0545

    Deleting post using index done

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello You"}
@@ -57,3 +63,15 @@ def get_post(id: int, response: Response):
         # return {"data": f'Post with the id: {id} does not exist'}
     print(post)
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Post with the id: {id} does not exist')
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
